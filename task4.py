021688ccab209ab37ac5e580e92e31dbdb1da14d
Ilya <buagir1990@gmail.com>
2019-06-24 10:07:41 +0300

    Fix code

--- task4.py
+++ task4.py
@@ -1,10 +1,11 @@
 a = int(input('Input your number: '))
-if a <= 99 and a >= 1:
+if a in range(1, 99):
     b = bin(a)[2:]
     s = len(b)
     i = 0
-    for i in range(0, a):
-        print(str(i).rjust(s) + oct(i)[2:].rjust(s) + hex(i)[2:].rjust(s) + bin(i)[2:].rjust(s))
+    k = " "
+    for i in range(1, a):
+        print(str(i).rjust(s) + k + oct(i)[2:].rjust(s) + k + hex(i)[2:].rjust(s) + k + bin(i)[2:].rjust(s))
         i += 1
 else:
     exit
