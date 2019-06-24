e62eee991d55d2cbad0d453214a5fd695ca43b71
Ilya <buagir1990@gmail.com>
2019-06-24 10:43:51 +0300

    Fix code
    Added tabs

--- task3.py
+++ task3.py
@@ -7,9 +7,9 @@ count = int(input())
 arr = [[0] * h for i in range(w)]
 for i in range(count):
     square = [int(l) for l in input().split()]
-for i in range(square[0], square[2]):
-    for j in range(square[1], square[3]):
-        if arr[i][j] == 0:
-            k += 1
-arr[i][j] = 1
+    for i in range(square[0], square[2]):
+        for j in range(square[1], square[3]):
+            if arr[i][j] == 0:
+                k += 1
+                arr[i][j] = 1
 print(canvasArea - k)
