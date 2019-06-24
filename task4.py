f8e298a911eb87ca350966a86a52ec6b2c8dedec
Ilya <buagir1990@gmail.com>
2019-06-24 10:27:22 +0300

    Fix code

--- task4.py
+++ task4.py
@@ -3,9 +3,8 @@ if a in range(1, 99):
     b = bin(a)[2:]
     s = len(b)
     i = 0
-    k = " "
     for i in range(1, a):
-        print(str(i).rjust(s) + k + oct(i)[2:].rjust(s) + k + hex(i)[2:].rjust(s) + k + bin(i)[2:].rjust(s))
+        print(str(i).rjust(s), oct(i)[2:].rjust(s), hex(i)[2:].rjust(s), bin(i)[2:].rjust(s))
         i += 1
 else:
     exit
