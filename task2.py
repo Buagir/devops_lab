c100aefb81f4b81317bda06f9efb38280fe47b9c
Ilya <buagir1990@gmail.com>
2019-06-30 22:39:10 +0300

    Fix problems

--- task2.py
+++ task2.py
@@ -1,6 +1,8 @@
-a = str(input())
-b = ''.join(reversed(a))
-if a == b:
-    print("Your word is polindrom")
-else:
-    print("Not polindrom")
+def rev():
+    a = str(input())
+    b = ''.join(reversed(a))
+    if a == b:
+        print("Your word is polindrom")
+    else:
+        print("Not polindrom")
+rev = rev()
